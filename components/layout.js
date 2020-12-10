a86e400dfdd8ba9f1a9570de9fff52191a3ecd20
ktsht <kitaa@oregonstate.edu>
2020-12-09 22:59:12 -0800

    Changed the title

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Atsuhito Kita'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Atsuhito Kita'
 
 export default function Layout({ children, home }) {
   return (
